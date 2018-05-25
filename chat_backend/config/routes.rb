198b70f2b335b082204f74b70a6c3bb34aa5162f
Nguyen Thanh Luan <n-luan@ruby-dev.jp>
2018-05-25 11:59:21 +0700

    backend login

---
+++ chat_backend/config/routes.rb
@@ -0,0 +1,4 @@
+Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'api/auth'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
---
+++ chat_backend/db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |n|
+  User.create email: "user#{n}@example.com", password: "123456789"
+end
